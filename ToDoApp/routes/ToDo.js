e0594a7adf60f89415a14308a9c9cc9e847be2ff
Ankit Dahiya <71880797+ankitd2002@users.noreply.github.com>
2023-11-21 13:13:28 -0800

    Add files via upload

--- ToDoApp/routes/ToDo.js
+++ ToDoApp/routes/ToDo.js
@@ -2,10 +2,18 @@ const express = require("express");
 const router = express.Router();
 
 //import controller
-const {createTodo} = require("../controllers/createToDo");
+const {createToDo} = require("../controllers/createToDo");
+const {getTodo,getToDoById} = require("../controllers/getToDo");
+const {updateToDo} = require("../controllers/updateToDo");
+const {deleteToDo} = require("../controllers/deleteToDo");
 
 //define api routes
-router.post("/createToDo",createTodo);
+router.post("/createToDo",createToDo);
+router.get("/getToDos",getTodo);
+router.get("/getToDos/:id",getToDoById);
+router.put("/updateToDo/:id",updateToDo);
+router.delete("/deleteToDo/:id",deleteToDo);
+
 
 module.exports = router;
 
